66c6bb7fb13eb719692a3608465c2ab596e9bd17
thamaraiselvam <thamaraiselvam@thamaraiselvams-MacBook-Pro.local>
2019-07-31 15:55:58 +0530

    added firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter/services.dart';
 import 'package:numbers/screens/GameScreen/GameScreen.dart';
 import 'package:numbers/screens/TurorialScreen.dart';
 import 'package:numbers/screens/LoadingScreen.dart';
 import 'package:numbers/screens/SplashScreen.dart';
 import 'package:numbers/screens/HomeScreen/HomeScreen.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 
 void main() {
@@ -18,6 +19,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
+  FirebaseAnalytics analytics = FirebaseAnalytics();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,6 +32,9 @@ class MyApp extends StatelessWidget {
         '/game': (BuildContext context) => GameScreen(),
         '/tutorial': (BuildContext context) => TurorialScreen(),
       },
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
       // navigatorObservers: <NavigatorObserver>[observer],
     );
   }
