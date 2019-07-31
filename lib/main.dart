03a717fbaa99804b2f4e565d577ef5315f7c3872
thamaraiselvam <thamaraiselvam@thamaraiselvams-MacBook-Pro.local>
2019-07-31 16:55:12 +0530

    firebase analytics added

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,28 @@ import 'package:numbers/screens/LoadingScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:numbers/screens/SplashScreen.dart';
 import 'package:numbers/screens/HomeScreen/HomeScreen.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 Future<void> main() async {
+    FirebaseAnalytics analytics = FirebaseAnalytics();
+
   SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     new MaterialApp(
-      initialRoute: '/',
-      routes: <String, WidgetBuilder> {
-        '/': (BuildContext context) => SplashScreen(),
-        '/home': (BuildContext context) => HomeScreen(),
-        '/loading': (BuildContext context) => LoadingScreen(),
-        '/game': (BuildContext context) => GameScreen(),
-        '/tutorial': (BuildContext context) => TurorialScreen(),
-      },
-    ));
+    initialRoute: '/',
+    routes: <String, WidgetBuilder> {
+      '/': (BuildContext context) => SplashScreen(),
+      '/home': (BuildContext context) => HomeScreen(),
+      '/loading': (BuildContext context) => LoadingScreen(),
+      '/game': (BuildContext context) => GameScreen(),
+      '/tutorial': (BuildContext context) => TurorialScreen(),
+    },
+    navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
+  ));
 }
