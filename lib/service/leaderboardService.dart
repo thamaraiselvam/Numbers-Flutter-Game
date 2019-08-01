a37560893bb77f0b401500e667e5164c349ae908
thamaraiselvam <thamaraiselvam@thamaraiselvams-MacBook-Pro.local>
2019-08-01 12:01:06 +0530

    renamed

--- lib/service/leaderboardService.dart
+++ lib/service/leaderboardService.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class leaderboardService {
+class LeaderboardService {
   final String collection = 'leaderboard';
   final String orderByKey = 'score';
 
