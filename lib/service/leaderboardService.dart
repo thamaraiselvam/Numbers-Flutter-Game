a0d0fac346b85118df5b882f4d2a70e8d8805a28
thamaraiselvam <thamaraiselvam@live.com>
2019-10-11 22:09:57 +0530

    weekly and daily leaderboard added

--- lib/service/leaderboardService.dart
+++ lib/service/leaderboardService.dart
@@ -5,6 +5,11 @@ import 'package:numbers/utils/Common.dart';
 class LeaderboardService {
   final String collection = 'leaderboard';
   final String orderByKey = 'score';
+  final Map<String, int> leaderBoardTypes = {
+    'overall': 0,
+    'weekly': 7,
+    'daily': 1
+  };
 
   setData(Map gameState) async {
     Map newData = await this._formatSetData(gameState);
@@ -12,10 +17,9 @@ class LeaderboardService {
   }
 
   _formatSetData(Map gameState) async {
-
     String name = await SettingsStore().getKey('name');
 
-    if(name == null){
+    if (name == null) {
       name = Common.getRandomName();
     }
 
@@ -30,28 +34,79 @@ class LeaderboardService {
     };
   }
 
-  getData({int limit = 6}) async {
+  getData({int limit = 6, leaderboardType = 'overall'}) async {
     try {
-      var resultData = await Firestore.instance
-          .collection(this.collection)
-          .orderBy(this.orderByKey, descending: true)
-          .limit(limit)
-          .getDocuments();
+      int daysToSubtract = this.leaderBoardTypes[leaderboardType];
+      var resultData;
 
-      return this._formatGetData(resultData);
+      if (daysToSubtract != 0) {
+        String lastWeekTimeStamp = DateTime.now()
+            .subtract(Duration(days: daysToSubtract))
+            .millisecondsSinceEpoch
+            .toString();
+
+        resultData = await _getDataByTimeCondition(lastWeekTimeStamp);
+
+      } else {
+        resultData = await _getOverallData(limit);
+      }
+
+      return this._formatGetData(resultData, limit);
     } catch (error) {
-      print(error);
+      //print(error);
     }
   }
 
-  _formatGetData(resultData) {
-    List formattedData = [];
+  _getDataByTimeCondition(String daysToSubtract) async {
+    return await Firestore.instance
+        .collection(this.collection)
+        .where("time", isGreaterThanOrEqualTo: daysToSubtract)
+        .getDocuments();
+  }
 
-    int counter = 0;
-    for (var document in resultData.documents) {
-      document.data['rank'] = ++counter;
-      formattedData.add(document.data);
+  _getOverallData(limit) async {
+    return await Firestore.instance
+        .collection(this.collection)
+        .orderBy(this.orderByKey, descending: true)
+        .limit(limit)
+        .getDocuments();
+  }
+
+  _formatGetData(collection, limit) {
+    List list = this._formList(collection);
+    _sortList(list);
+    list = _addRank(list);
+    return _splitList(list, 0, limit);
+  }
+
+  List _formList(collection) {
+    List list = [];
+    for (var document in collection.documents) {
+      list.add(document.data);
     }
-    return formattedData;
+
+    return list;
+  }
+
+  _sortList(List documents) {
+    documents.sort((m1, m2) {
+      return m2["score"].compareTo(m1["score"]);
+    });
+  }
+
+  List _addRank(List documents) {
+    int counter = 0;
+    for (var document in documents) {
+      document['rank'] = ++counter;
+    }
+
+    return documents;
+  }
+
+  List _splitList(List list, int start, int end) {
+    if (list.length < end) {
+      end = list.length;
+    }
+    return list.sublist(start, end);
   }
 }
